529c3f53a4eba9ceedb7a7d498124b0076f27dd8
Narendra Varma <narendandu@gmail.com>
2025-06-09 14:09:38 -0700

    RDKB-58489: Support to configure gcmp
    Reason for change: support to enable/disable gcmp
    		   for encryption.
    
    Test Procedure: 1. Build images.
    		2. Enable 11be and configure security in WPA3-PT
    		3. Change Security.X_CISCO_COM_EncryptionMethod to AES
    		4. Test wifi6 and wifi7 client connections.
    		5. Change Security.X_CISCO_COM_EncryptionMethod to
    		   AES+GCMP
    		6. Test wifi6 and wifi7 client connections.
    		7. Repeat steps 2 to 6 with secuirty as WPA3-P
    Risks: Medium
    Priority: P1

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -6681,23 +6681,29 @@ int wifidb_init_vap_config_default(int vap_index, wifi_vap_info_t *config,
 
     if (isVapSTAMesh(vap_index)) {
         cfg.vap_mode = wifi_vap_mode_sta;
+        cfg.u.sta_info.security.encr = wifi_encryption_aes;
         if (band == WIFI_FREQUENCY_6_BAND) {
             cfg.u.sta_info.security.mode = wifi_security_mode_wpa3_personal;
             cfg.u.sta_info.security.wpa3_transition_disable = true;
             cfg.u.sta_info.security.mfp = wifi_mfp_cfg_required;
             cfg.u.sta_info.security.u.key.type = wifi_security_key_type_sae;
+#ifdef CONFIG_IEEE80211BE
+                cfg.u.sta_info.security.encr = wifi_encryption_aes_gcmp256;
+#endif /* CONFIG_IEEE80211BE */
         } else {
 #if defined(NEWPLATFORM_PORT)
                 cfg.u.sta_info.security.mode = wifi_security_mode_wpa3_transition;
                 cfg.u.sta_info.security.wpa3_transition_disable = false;
                 cfg.u.sta_info.security.mfp = wifi_mfp_cfg_optional;
                 cfg.u.sta_info.security.u.key.type = wifi_security_key_type_psk_sae;
+#ifdef CONFIG_IEEE80211BE
+                cfg.u.sta_info.security.encr = wifi_encryption_aes_gcmp256;
+#endif /* CONFIG_IEEE80211BE */
 #else
                 cfg.u.sta_info.security.mfp = wifi_mfp_cfg_disabled;
                 cfg.u.sta_info.security.mode = wifi_security_mode_wpa2_personal;
 #endif
         }
-        cfg.u.sta_info.security.encr = wifi_encryption_aes;
         cfg.u.sta_info.enabled = false;
         cfg.u.sta_info.scan_params.period = 10;
         memset(ssid, 0, sizeof(ssid));
@@ -6807,53 +6813,68 @@ int wifidb_init_vap_config_default(int vap_index, wifi_vap_info_t *config,
             }
         } else if (isVapHotspotSecure(vap_index)) {
             cfg.u.bss_info.bssHotspot = true;
+            cfg.u.bss_info.security.encr = wifi_encryption_aes;
             if (band == WIFI_FREQUENCY_6_BAND) {
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa3_enterprise;
                 cfg.u.bss_info.security.mfp = wifi_mfp_cfg_required;
+#ifdef CONFIG_IEEE80211BE
+                cfg.u.bss_info.security.encr = wifi_encryption_aes_gcmp256;
+#endif /* CONFIG_IEEE80211BE */
             }
             else {
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa2_enterprise;
             }
-            cfg.u.bss_info.security.encr = wifi_encryption_aes;
         } else if (isVapLnfSecure (vap_index)) {
             cfg.u.bss_info.security.mode = wifi_security_mode_wpa2_enterprise;
             cfg.u.bss_info.security.encr = wifi_encryption_aes;
         } else if (isVapPrivate(vap_index))  {
+            cfg.u.bss_info.security.encr = wifi_encryption_aes;
             if (band == WIFI_FREQUENCY_6_BAND) {
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa3_personal;
                 cfg.u.bss_info.security.wpa3_transition_disable = true;
                 cfg.u.bss_info.security.mfp = wifi_mfp_cfg_required;
                 cfg.u.bss_info.security.u.key.type = wifi_security_key_type_sae;
+#ifdef CONFIG_IEEE80211BE
+                cfg.u.bss_info.security.encr = wifi_encryption_aes_gcmp256;
+#endif /* CONFIG_IEEE80211BE */
             } else {
 #if defined(_XB8_PRODUCT_REQ_) || defined(_SR213_PRODUCT_REQ_) || defined(_XER5_PRODUCT_REQ_) || defined(_SCER11BEL_PRODUCT_REQ_)
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa3_transition;
                 cfg.u.bss_info.security.wpa3_transition_disable = false;
                 cfg.u.bss_info.security.mfp = wifi_mfp_cfg_optional;
                 cfg.u.bss_info.security.u.key.type = wifi_security_key_type_psk_sae;
+#ifdef CONFIG_IEEE80211BE
+                cfg.u.bss_info.security.encr = wifi_encryption_aes_gcmp256;
+#endif /* CONFIG_IEEE80211BE */
 #else
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa2_personal;
 #endif
             }
-            cfg.u.bss_info.security.encr = wifi_encryption_aes;
             cfg.u.bss_info.bssHotspot = false;
             cfg.u.bss_info.mbo_enabled = false;
         } else  {
+            cfg.u.bss_info.security.encr = wifi_encryption_aes;
             if (band == WIFI_FREQUENCY_6_BAND) {
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa3_personal;
                 cfg.u.bss_info.security.wpa3_transition_disable = true;
                 cfg.u.bss_info.security.mfp = wifi_mfp_cfg_required;
                 cfg.u.bss_info.security.u.key.type = wifi_security_key_type_sae;
+#ifdef CONFIG_IEEE80211BE
+                cfg.u.bss_info.security.encr = wifi_encryption_aes_gcmp256;
+#endif /* CONFIG_IEEE80211BE */
             } else {
 #if defined(NEWPLATFORM_PORT)
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa3_transition;
                 cfg.u.bss_info.security.wpa3_transition_disable = false;
                 cfg.u.bss_info.security.mfp = wifi_mfp_cfg_optional;
                 cfg.u.bss_info.security.u.key.type = wifi_security_key_type_psk_sae;
+#ifdef CONFIG_IEEE80211BE
+                cfg.u.bss_info.security.encr = wifi_encryption_aes_gcmp256;
+#endif /* CONFIG_IEEE80211BE */
 #else
                 cfg.u.bss_info.security.mode = wifi_security_mode_wpa2_personal;
 #endif
             }
-            cfg.u.bss_info.security.encr = wifi_encryption_aes;
             cfg.u.bss_info.bssHotspot = false;
         }
         cfg.u.bss_info.beaconRate = WIFI_BITRATE_6MBPS;
